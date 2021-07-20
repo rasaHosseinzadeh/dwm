0d88804dc065ba9acf1f4b104757449cd8520334
Rasa Hosseinzadeh <rasa.hosseinzadeh@gmail.com>
2021-07-19 21:57:44 -0400

    Add floating rule for dmenu.

--- config.def.h
+++ config.def.h
@@ -45,6 +45,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance           title       tags mask       isfloating     monitor */
+	{ "dmenu",       NULL,           NULL,       0,                   1,           -1,  },
 	{ "mpv",         NULL,           NULL,       0,                   1,           -1,  },
 	{ NULL,          "sxiv",         NULL,       0,                   1,           -1,  },
 	{ NULL,          NULL,           "sxiv",     0,                   1,           -1,  },
